8b4481ec1ff01d75cde39a9a052ae2195a67f4dd
Ehsan Faisal <ehsanfaisal189@gmail.com>
2025-11-21 15:17:55 +0000

    es project pa jo naam hai na us kai saath id lagao api theek hai meri

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,9 +4,9 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyCV3Tz_b2h6e9m5g2Q7fP9v5q9l3c9j7M",
-  authDomain: "studio-project-staging-2024.firebaseapp.com",
-  projectId: "studio-project-staging-2024",
-  storageBucket: "studio-project-staging-2024.appspot.com",
+  authDomain: "ehsan-study-website.firebaseapp.com",
+  projectId: "ehsan-study-website",
+  storageBucket: "ehsan-study-website.appspot.com",
   messagingSenderId: "987654321012",
   appId: "1:987654321012:web:a1b2c3d4e5f6a7b8c9d0e1"
 };
